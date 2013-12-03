d264ea51c49941a575a99181cd29e0af2d73246e
Renzo Trigoso <renzotrigoso@gmail.com>
2013-12-02 21:13:33 -0500

    added stacks generic implementation

---
+++ stack.adb
@@ -0,0 +1,43 @@
+package body stack is
+
+  SIZE_CONSTANT	: constant positive 	:= SIZE;
+
+  subtype STACK_DATA is positive range 1 .. SIZE_CONSTANT;
+
+  STACK_ARRAY	: array (STACK_DATA) of DATA_TYPE;
+  STACK_POINTER	: STACK_DATA	 	:= 1;
+
+  -- generic procedure pop
+  procedure Push (DATA : DATA_TYPE) is
+    begin
+      if STACK_POINTER 			= SIZE_CONSTANT then 	-- if full raise overflow_error
+        raise OVERFLOW_ERROR;
+      else							-- if space is open add to stack
+        STACK_ARRAY(STACK_POINTER) 	:= DATA;
+        STACK_POINTER 			:= STACK_POINTER + 1;
+      end if;
+  end Push;
+
+  -- generic function pop
+  function Pop return DATA_TYPE is
+    begin
+      if STACK_POINTER 			= 1 then		-- if not enough data raise underflow_error
+        raise UNDERFLOW_ERROR;
+      else							-- if data exists then return said data
+        STACK_POINTER 			:= STACK_POINTER - 1;
+        return STACK_ARRAY(STACK_POINTER);
+      end if;
+   end Pop;
+
+  -- generic procedure empty
+  procedure Empty is
+    begin
+      STACK_POINTER := 1; 					-- clears all data
+  end Empty;
+
+  function Is_Empty return Boolean is
+    begin
+      return STACK_POINTER = 1;					-- returns true if no items on stack
+   end Is_Empty;
+
+end stack;
--- stack.ads
+++ stack.ads
@@ -2,11 +2,15 @@
 
 generic
     type DATA_TYPE is private;
+    SIZE : positive;
 
 package stack is
 
+-- exceptions
+OVERFLOW_ERROR, UNDERFLOW_ERROR : exception;
+
 -- push
-procedure Push is (DATA : DATA_TYPE);
+procedure Push (DATA : DATA_TYPE);
 
 -- pop
 function Pop return DATA_TYPE; 
@@ -16,3 +20,5 @@ procedure Empty;
 
 -- is_empty
 function Is_Empty return BOOLEAN;
+
+end stack;
